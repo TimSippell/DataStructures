4fb20463b14b84bc7a78ba2f00f65e360d9646c4
Tim Sippell <tjsippell@gmail.com>
2018-05-03 19:43:58 -0400

    stack

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DataStructures.Stack;
 
 namespace DataStructures
 {
---
+++ DataStructures/Stack/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DataStructures.Stack
+{
+    public class Stack<T>
+    {
+        private Node _head;
+
+        public Stack(T t)
+        {
+            _head = new Node(t);
+        }
+
+        public void Push(T t)
+        {
+            var temp = new Node(t) {Next = _head};
+            _head = temp;
+        }
+
+        public T Pop()
+        {
+            try
+            {
+                var temp = _head;
+                _head = _head.Next;
+                return temp.Data;
+            }
+            catch
+            {
+                throw new Exception("Stack is empty");
+            }
+        }
+
+        private class Node
+        {
+            public Node Next { get; set; }
+            public T Data { get; private set; }
+
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+    }
+}
